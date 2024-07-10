7f9ccb4a182ce3ac422691b61378d46a4f94c7e9
Sonia <64159695+soniaveit@users.noreply.github.com>
2024-07-09 23:28:52 -0300

    Update conexionAPI.js
    Realizada limpieza de código. Quité comentarios no relevantes.

--- js/conexionAPI.js
+++ js/conexionAPI.js
@@ -2,16 +2,11 @@ async function listarCuadros(){
     //const conexion= await fetch("http://localhost:3001/cuadros");
     const conexion= await fetch("https://alura-geek-api-delta.vercel.app/cuadros");
     const conexionConvertida=conexion.json();
-    //console.log(conexionConvertida);
-
     return conexionConvertida;
 };
 
 //Aquí debemos indicar el método y qué tipo de archivo se estará enviando
-//eso en headers, y en body, el cuerpo del mensaje
 async function enviarCuadro(titulo, tecnica, imagen){
-    //const conexion = await fetch("http://localhost:3001/cuadros",{
-    //const conexion= await fetch("https://alura-geek-api-delta.vercel.app/cuadros",{
     return await fetch("https://alura-geek-api-delta.vercel.app/cuadros",{
         method: "POST",
         headers:{"Content-type":"application/json"},
@@ -21,14 +16,10 @@ async function enviarCuadro(titulo, tecnica, imagen){
             imagen:imagen
         })
     })
-    //const conexionConvertida=conexion.json();
-    
-    //return conexionConvertida;
 };
 
 const borrarCuadro = async (id) => {
     try{
-        //const res= await fetch(`http://localhost:3001/cuadros/${id}`,{
         const res= await fetch(`https://alura-geek-api-delta.vercel.app/cuadros/${id}`,{
             method: "DELETE"
         });
@@ -37,19 +28,9 @@ const borrarCuadro = async (id) => {
         return console.log(err);
     }
 }
-/*async function borrarCuadro(id){
-    //return await fetch(`http://localhost:3001/cuadros/${id}`,{
-    return await fetch(`https://alura-geek-api-delta.vercel.app/cuadros/${id}`,{
-        method: "DELETE"
-    })
-    .then(res => res.json())
-    .catch((err) => console.log(err));
-};*/
 
 export const conexionApi={
     listarCuadros,
     enviarCuadro,
     borrarCuadro
 }
-
-//listarCuadros();
